3a85ed6579478e993574cf869f5b7f9958ce140f
Jose Maiorini <josemaiorini@gmail.com>
2021-11-02 09:16:31 -0300

    varios 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola Mundo, soy {nombre} <br/>
+          Hola Mundo, soy {nombre} Maiorini<br/>
         </p>
         <a
           className="App-link"
